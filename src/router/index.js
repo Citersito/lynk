1718021775a46ee6cf161f235af78c4751d252f8
Citer <citer5@hotmail.com>
2024-11-26 23:16:24 -0600

    mi pagina

--- src/router/index.js
+++ src/router/index.js
@@ -2,11 +2,13 @@ import { createRouter, createWebHistory } from "vue-router";
 import ProductPage from "@/views/ProductPage.vue";
 import LayoutApp from "@/components/LayoutApp.vue";
 import PedidosPage from "@/views/PedidosPage.vue";
+import MyPage from "@/views/MyPage.vue";
 
 const routes = [
 	{ path: "/", component: LayoutApp },
 	{ path: "/productos", component: ProductPage },
 	{ path: "/pedidos", component: PedidosPage },
+	{ path: "/mi-pagina", component: MyPage },
 ];
 
 const router = createRouter({

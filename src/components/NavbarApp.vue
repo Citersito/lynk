e08798bdeeb8a70aa73e89715c51a4dbcb434dff
Citer <citer5@hotmail.com>
2024-11-26 22:56:08 -0600

    navbar ruteada

--- src/components/NavbarApp.vue
+++ src/components/NavbarApp.vue
@@ -1,11 +1,21 @@
 <template>
 	<div id="navbar">
-		<div id="navbarTitle">
-			<h1>Lynk</h1>
-		</div>
-		<h2>Productos</h2>
-		<h2>Pedidos</h2>
-		<h2>Mi Pagina</h2>
-		<h2>Configuración</h2>
+		<router-link to="/home">
+			<div id="navbarTitle">
+				<h1>Lynk</h1>
+			</div></router-link
+		>
+		<router-link to="/productos">
+			<h2>Productos</h2>
+		</router-link>
+		<router-link to="/pedidos">
+			<h2>Pedidos</h2>
+		</router-link>
+		<router-link to="/mi-pagina">
+			<h2>Mi Página</h2>
+		</router-link>
+		<router-link to="/configuracion">
+			<h2>Configuración</h2>
+		</router-link>
 	</div>
 </template>

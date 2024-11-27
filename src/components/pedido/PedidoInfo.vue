981d059f3297e6983687ab63b8420956e3cd2fdf
Citer <citer5@hotmail.com>
2024-11-26 23:06:11 -0600

    pagina de pedidos

---
+++ src/components/pedido/PedidoInfo.vue
@@ -0,0 +1,17 @@
+<template>
+	<div class="pedido">
+		<div class="pedido-info">
+			<h3 class="pedido-id">ID del Pedido</h3>
+			<p class="pedido-cliente">Cliente</p>
+			<p class="pedido-total">Total</p>
+			<p class="pedido-fecha">Fecha de Pedido</p>
+		</div>
+		<div class="pedido-acciones">
+			<button class="pedido-ver">Ver pedido</button>
+			<button class="pedido-estatus">Cambiar Estatus</button>
+		</div>
+	</div>
+</template>
+<script>
+export default {};
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,12 @@
 import { createRouter, createWebHistory } from "vue-router";
 import ProductPage from "@/views/ProductPage.vue";
 import LayoutApp from "@/components/LayoutApp.vue";
+import PedidosPage from "@/views/PedidosPage.vue";
 
 const routes = [
 	{ path: "/", component: LayoutApp },
 	{ path: "/productos", component: ProductPage },
+	{ path: "/pedidos", component: PedidosPage },
 ];
 
 const router = createRouter({
